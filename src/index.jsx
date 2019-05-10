727cf3160195c656c73b72ac0be37fe1ab4b9d90
Student <example@example.com>
2019-05-10 10:51:18 -0700

    update index.jsx file with react-redux Provider tag

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import dispatchReducer from './reducers/dispatch-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(dispatchReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
+
       <HashRouter>
-        <Component/>
-      </HashRouter>
-    </AppContainer>,
+        <Provider store={store}>
+          <Component/>
+        </Provider>
+      </HashRouter>,
+
     document.getElementById('react-app-root')
   );
 };
