29f3bd43d485fba368f5dd48036b0f07022015ce
Student <example@example.com>
2019-05-10 10:02:32 -0700

    add mock data to test

--- _tests_/dispatcher-reducer.test.js
+++ _tests_/dispatcher-reducer.test.js
@@ -1,9 +1,18 @@
-import dispatcherReducer from './..src/reducers/dispatcher-reducer'
+import dispatcherReducer from './../src/reducers/dispatcher-reducer'
 
 describe('dispatcherReducer', () => {
 
+  let action;
+  const sampleTicketData = {      {
+      firstName: 'Jack',
+      lastName: 'Ofalltrades',
+      email: 'BeNimble420@gmail.com',
+      notesMaster: '',
+      id: 'mock1'
+    };
+
   test('Should return default state if no action type is recognized', () => {
-    expect(ticketListReducer({}, { type: null })).toEqual({});
+    expect(dispatcherReducer({}, { type: null })).toEqual({});
   });
 
 });
