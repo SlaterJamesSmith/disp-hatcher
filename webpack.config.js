85345ac6bdd7505c29a34a4888b4288acfad899a
Slater Smith <smithslater@gmail.com>
2019-05-27 22:12:00 -0700

    update webpack.config.js project title name

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Template',
+      title: 'DispHatcher',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
